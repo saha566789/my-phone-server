0595ddf233063407e13a7afa5f985ce2b3e8b525
saha566789 <tomasaha3456@gmail.com>
2023-10-10 17:07:01 +0600

    first commit

---
+++ src/componets/Phone.jsx
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const Phone = () => {
+    const phone = useLoaderData()
+    console.log(phone)
+    return (
+        <div>
+            <h2>{phone.name}</h2>
+            
+        </div>
+    );
+};
+
+export default Phone;
